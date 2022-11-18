0ceec50643cdb6699c0dc0599520667ccccc5ff4
Seth Daulton <seth.daulton@gmail.com>
2022-11-17 21:54:40 -0600

    added information to pages, created classes, created base code for employees, managers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+function generateEmployee() {
+  // generate employee and add to 
+}
+
 const generateHTML = ({ name, title, id, email, officeNumber,
-    name2, title2, id2, email2, github,
-    name3, title3, id3, email3, github2,
-    name4, title4, id4, email4, github3,
+    name2, title2, id2, email2, githubUrl, github,
+    name3, title3, id3, email3, githubUrl2, github2,
+    name4, title4, id4, email4, githubUrl3, github3,
     name5, title5, id5, email5, school }) =>
   `<meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -47,10 +51,10 @@ const generateHTML = ({ name, title, id, email, officeNumber,
           <p>ID: ${id2}</p>
       </div>
       <div class="info-container">
-          <p>Email: ${email2}</p>
+      <p>Email: <a href = "mailto: ${email2}">${email2}</a></p>
       </div>
       <div class="info-container">
-          <p>GitHub: ${github}</p>
+          <p>GitHub: <a href="${githubUrl}">${github}</a></p>
       </div>
   </div>
   </div>
@@ -65,10 +69,10 @@ const generateHTML = ({ name, title, id, email, officeNumber,
           <p>ID: ${id3}</p>
       </div>
       <div class="info-container">
-          <p>Email: ${email3}</p>
+      <p>Email: <a href = "mailto: ${email3}">${email3}</a></p>
       </div>
       <div class="info-container">
-          <p>GitHub: ${github2}</p>
+      <p>GitHub: <a href="${githubUrl2}">${github2}</a></p>
       </div>
   </div>
   </div>
@@ -83,10 +87,10 @@ const generateHTML = ({ name, title, id, email, officeNumber,
           <p>ID: ${id4}</p>
       </div>
       <div class="info-container">
-          <p>Email: ${email4}</p>
+      <p>Email: <a href = "mailto: ${email4}">${email4}</a></p>
       </div>
       <div class="info-container">
-          <p>GitHub: ${github3}</p>
+      <p>GitHub: <a href="${githubUrl3}">${github3}</a></p>
       </div>
   </div>
   </div>
@@ -101,7 +105,7 @@ const generateHTML = ({ name, title, id, email, officeNumber,
           <p>ID: ${id5}</p>
       </div>
       <div class="info-container">
-          <p>Email: ${email5}</p>
+      <p>Email: <a href = "mailto: ${email5}">${email5}</a></p>
       </div>
       <div class="info-container">
           <p>School: ${school}</p>
@@ -120,38 +124,63 @@ inquirer
       name: 'name',
       message: 'What is the name of the manager?',
     },
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What title is this employee?',
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is this employee's id number?",
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What is this employee's email address?",
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: "What is this employee's office number?",
-    },
-    // Questions for Employee #2
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: "What is this employee's office number?",
-      },
+    // {
+    //   type: 'input',
+    //   name: 'title',
+    //   message: 'What title is this employee?',
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'id',
+    //   message: "What is this employee's id number?",
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'email',
+    //   message: "What is this employee's email address?",
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'officeNumber',
+    //   message: "What is this employee's office number?",
+    // },
+    // // Questions for Employee #2
+    // {
+    //     type: 'input',
+    //     name: 'name2',
+    //     message: "What is this name of the engineer?",
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'title2',
+    //     message: "What is this employee's title?",
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'id2',
+    //     message: "What is this employee's id?",
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'email2',
+    //     message: "What is this employee's email address?",
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'githubUrl',
+    //     message: "What is the link to the employee's github profile?",
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'github',
+    //     message: "What is the employee's github profile name?",
+    //   },
 
     
   ])
   .then((answers) => {
     const htmlPageContent = generateHTML(answers);
-
+    console.log(answers);
     fs.writeFile('index.html', htmlPageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
     );
